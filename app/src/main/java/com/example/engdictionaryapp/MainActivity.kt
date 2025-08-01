a0020de5af9a717bec358ddba29a306e376c07d6
Roman <chailoshkarev@mail.ru>
2025-08-02 01:27:18 +0300

    fix bugs with compose

--- app/src/main/java/com/example/engdictionaryapp/MainActivity.kt
+++ app/src/main/java/com/example/engdictionaryapp/MainActivity.kt
@@ -1,32 +1,8 @@
 package com.example.engdictionaryapp
 
 import android.os.Bundle
-import android.util.Log
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
-import com.example.engdictionaryapp.databinding.ActivityLearnWordBinding
-import com.example.engdictionaryapp.trainer.domain.LearnWordTrainer
-import com.example.engdictionaryapp.ui.AnswerIndicator
-import com.example.engdictionaryapp.ui.CloseScreen
-import com.example.engdictionaryapp.ui.util.QuestionIndicator
-import com.example.engdictionaryapp.ui.util.VariantViewHelper
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.observeOn
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
